6032cd8b27145bce091e8e6c318ace7e12e8e82f
Andi Rachmadian <andikharisma24@gmail.com>
2020-04-18 16:49:44 +0700

    add input and button sign in

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 const Signin = () => {
   return (
-    <div>
+    <div className='container'>
       <h1>Halaman Login</h1>
+      <div className='form-group'>
+        <label className='control-label'>Email</label>
+        <input className='form-control' />
+      </div>
+      <div className='form-group'>
+        <label className='control-label'>Password</label>
+        <input className='form-control' type='password' />
+      </div>
+      <button className='btn-primary' type='submit'>
+        Sign in
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Signin;
