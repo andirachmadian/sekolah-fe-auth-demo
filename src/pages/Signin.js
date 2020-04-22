43d99076505be55a80042e7163ef21c2c33545f0
Andi Rachmadian <andikharisma24@gmail.com>
2020-04-22 19:18:28 +0700

    edit signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
+import { useAlert } from 'react-alert';
 
-const Signin = () => {
+const Signin = (props) => {
+  const alert = useAlert();
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onChangeLogin = (e, field) => {
+    const newData = Object.assign({}, loginData);
+    newData[field] = e.target.value;
+
+    setLoginData(newData);
+  };
+
+  const onSubmitLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://service-st-subee.tabspace.xyz/external/users/signin', loginData);
+      alert.success('berhasil login');
+    } catch (error) {
+      alert.error('login galal');
+    }
+  };
+
   return (
     <div className='container'>
       <h1>Halaman Login</h1>
-      <div className='form-group'>
-        <label className='control-label'>Email</label>
-        <input className='form-control' />
-      </div>
-      <div className='form-group'>
-        <label className='control-label'>Password</label>
-        <input className='form-control' type='password' />
-      </div>
-      <button className='btn-primary' type='submit'>
-        Sign in
-      </button>
+      <form onSubmit={onSubmitLogin}>
+        <div className='form-group'>
+          <label className='control-label'>Email</label>
+          <input className='form-control' value={loginData.email} onChange={(e) => onChangeLogin(e, 'email')} />
+        </div>
+        <div className='form-group'>
+          <label className='control-label'>Password</label>
+          <input className='form-control' type='password' value={loginData.password} onChange={(e) => onChangeLogin(e, 'password')} />
+        </div>
+        <button className='btn-primary' type='submit'>
+          Sign in
+        </button>
+      </form>
     </div>
   );
 };
 
-export default Signin;
+export default withRouter(Signin);
